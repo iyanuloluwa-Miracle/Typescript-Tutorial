b6df8434bed5a26f8eceb1afd69586433a4d34eb
iyanuloluwa-Miracle <iyanudina@gmail.com>
2024-05-29 09:34:26 +0100

    :zap : Function Basics

--- public/sandbox.js
+++ public/sandbox.js
@@ -1,2 +1,14 @@
 "use strict";
-console.log('test');
+let greet;
+greet = () => {
+    console.log('hello, again');
+};
+const add = (a, b, c = 10) => {
+    console.log(a + b);
+    console.log(c);
+};
+add(5, 10, '20');
+const minus = (a, b) => {
+    return a + b;
+};
+let result = minus(10, 7);
