3d8cc7846834f962d5f6fcf49b42d842079f5e09
iyanuloluwa-Miracle <iyanudina@gmail.com>
2024-05-31 14:33:49 +0100

    :zap: Learning classes

--- public/app.js
+++ public/app.js
@@ -1,11 +1,29 @@
 "use strict";
-const form = document.querySelector('.new-item-form');
-console.log(form.children);
-const type = document.querySelector('#type');
-const tofrom = document.querySelector('#tofrom');
-const details = document.querySelector('#details');
-const amount = document.querySelector('#amount');
-form.addEventListener('submit', (e) => {
-    e.preventDefault();
-    console.log(type.value, tofrom.value, details.value, amount.valueAsNumber);
-});
+class Invoice {
+    constructor(c, d, a) {
+        this.client = c;
+        this.details = d;
+        this.amount = a;
+    }
+    format() {
+        return `${this.client} owes ${this.amount}`;
+    }
+}
+const invOne = new Invoice('mario', 'work on the mario website', 250);
+const invTwo = new Invoice('luigi', 'work on the luigi website', 350);
+console.log(invOne, invTwo);
+// const form = document.querySelector('.new-item-form') as HTMLFormElement;
+// console.log(form.children);
+// const type = document.querySelector('#type') as HTMLSelectElement;
+// const tofrom = document.querySelector('#tofrom') as HTMLInputElement;
+// const details = document.querySelector('#details') as HTMLInputElement;
+// const amount = document.querySelector('#amount') as HTMLInputElement;
+// form.addEventListener('submit', (e: Event) =>{
+//     e.preventDefault();
+//     console.log(
+//         type.value,
+//         tofrom.value,
+//         details.value,
+//         amount.valueAsNumber
+//     )
+// })
--- src/app.ts
+++ src/app.ts
@@ -1,20 +1,43 @@
-const form = document.querySelector('.new-item-form') as HTMLFormElement;
-console.log(form.children);
+class Invoice {
+    client: string;
+    details: string;
+    amount: number;
+
+    constructor(c: string, d:string, a:number){
+        this.client = c;
+        this.details = d;
+        this.amount = a;
+
+    }
+    format(){
+        return `${this.client} owes ${this.amount}`
+    }
+}
+
+const invOne = new Invoice('mario','work on the mario website', 250)
+const invTwo = new Invoice('luigi','work on the luigi website', 350)
+
+console.log(invOne, invTwo)
 
 
 
-const type = document.querySelector('#type') as HTMLSelectElement;
-const tofrom = document.querySelector('#tofrom') as HTMLInputElement;
-const details = document.querySelector('#details') as HTMLInputElement;
-const amount = document.querySelector('#amount') as HTMLInputElement;
+// const form = document.querySelector('.new-item-form') as HTMLFormElement;
+// console.log(form.children);
 
-form.addEventListener('submit', (e: Event) =>{
-    e.preventDefault();
 
-    console.log(
-        type.value,
-        tofrom.value,
-        details.value,
-        amount.valueAsNumber
-    )
-})
+
+// const type = document.querySelector('#type') as HTMLSelectElement;
+// const tofrom = document.querySelector('#tofrom') as HTMLInputElement;
+// const details = document.querySelector('#details') as HTMLInputElement;
+// const amount = document.querySelector('#amount') as HTMLInputElement;
+
+// form.addEventListener('submit', (e: Event) =>{
+//     e.preventDefault();
+
+//     console.log(
+//         type.value,
+//         tofrom.value,
+//         details.value,
+//         amount.valueAsNumber
+//     )
+// })
